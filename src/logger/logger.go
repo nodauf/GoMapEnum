77fc89a641357828e144562d58996053b3cb9689
nodauf <nodauf@krutt.org>
2022-05-05 15:50:58 +0400

    Update logger to allow to save to a file and disable color

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -21,23 +21,35 @@ const (
 
 // Logger is the options for the logging module
 type Logger struct {
-	Level  Level
-	Module string
-	Target string
-	Type   string
+	Level   Level
+	NoColor bool
+	Mode    string
+	Module  string
+	Target  string
+	File    string
 }
 
 // New create a Logger object for the specified arguments
-func New(kind, module, target string) *Logger {
+func New(mode, module, target string) *Logger {
 	log := &Logger{}
 	// If not set during the init of logger
 	if log.Level == 0 {
 		log.Level = InfoLevel
 	}
+	log.Mode = mode
 	log.Module = module
 	log.Target = target
-	log.Type = kind
 	return log
+}
+
+// SetTarget update the target field
+func (logger *Logger) SetTarget(target string) {
+	logger.Target = target
+}
+
+// SetModule update the module field
+func (logger *Logger) SetModule(module string) {
+	logger.Module = module
 }
 
 // SetLevel change the logging level
@@ -47,9 +59,16 @@ func (logger *Logger) SetLevel(level Level) {
 
 // Debug print with debug output
 func (logger *Logger) Debug(format string, a ...interface{}) {
+
 	if logger.Level >= DebugLevel {
+		var output string
 		str := fmt.Sprintf(format, a...)
-		output := color.HiMagentaString(str)
+		if logger.NoColor {
+			output = str
+		} else {
+			output = color.HiBlackString("[Debug] " + str)
+		}
+
 		logger.print(output)
 	}
 }
@@ -57,8 +76,13 @@ func (logger *Logger) Debug(format string, a ...interface{}) {
 // Verbose print with verbose output
 func (logger *Logger) Verbose(format string, a ...interface{}) {
 	if logger.Level >= VerboseLevel {
+		var output string
 		str := fmt.Sprintf(format, a...)
-		output := color.HiYellowString(str)
+		if logger.NoColor {
+			output = str
+		} else {
+			output = color.HiYellowString(str)
+		}
 		logger.print(output)
 	}
 }
@@ -66,8 +90,13 @@ func (logger *Logger) Verbose(format string, a ...interface{}) {
 // Info print with info output
 func (logger *Logger) Info(format string, a ...interface{}) {
 	if logger.Level >= InfoLevel {
+		var output string
 		str := fmt.Sprintf(format, a...)
-		output := color.HiYellowString(str)
+		if logger.NoColor {
+			output = str
+		} else {
+			output = color.HiYellowString(str)
+		}
 		logger.print(output)
 	}
 }
@@ -75,8 +104,13 @@ func (logger *Logger) Info(format string, a ...interface{}) {
 // Error print with error output
 func (logger *Logger) Error(format string, a ...interface{}) {
 	if logger.Level >= ErrorLevel {
+		var output string
 		str := fmt.Sprintf(format, a...)
-		output := color.HiRedString("/!\\ " + str)
+		if logger.NoColor {
+			output = str
+		} else {
+			output = color.HiRedString("/!\\ Error: " + str)
+		}
 		logger.print(output)
 	}
 }
@@ -84,8 +118,13 @@ func (logger *Logger) Error(format string, a ...interface{}) {
 // Fatal print with fatal output
 func (logger *Logger) Fatal(format string, a ...interface{}) {
 	if logger.Level >= FatalLevel {
+		var output string
 		str := fmt.Sprintf(format, a...)
-		output := color.HiRedString("/!\\ " + str)
+		if logger.NoColor {
+			output = str
+		} else {
+			output = color.HiRedString("/!\\ " + str)
+		}
 		logger.print(output)
 	}
 	os.Exit(1)
@@ -94,17 +133,28 @@ func (logger *Logger) Fatal(format string, a ...interface{}) {
 // Success print with success output (mostly when password is true or user exists)
 func (logger *Logger) Success(format string, a ...interface{}) {
 	if logger.Level >= InfoLevel {
+		var output string
 		str := fmt.Sprintf(format, a...)
-		output := color.HiGreenString("[+] " + str)
+		if logger.NoColor {
+			output = str
+		} else {
+			output = color.HiGreenString("[+] " + str)
+		}
 		logger.print(output)
 	}
 }
 
 // Fail print with fail output (mostly when password is wrong or user does not exist)
 func (logger *Logger) Fail(format string, a ...interface{}) {
+
 	if logger.Level >= VerboseLevel {
+		var output string
 		str := fmt.Sprintf(format, a...)
-		output := color.HiRedString("[-] " + str)
+		if logger.NoColor {
+			output = str
+		} else {
+			output = color.HiRedString("[-] " + str)
+		}
 		logger.print(output)
 	}
 }
--- src/logger/private.go
+++ src/logger/private.go
@@ -2,14 +2,39 @@ package logger
 
 import (
 	"fmt"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/fatih/color"
 )
 
 func (logger *Logger) print(str string) {
-	output := color.HiCyanString(logger.Type+" - "+logger.Module) + "\t\t" + logger.Target + "\t\t" + str + "\n"
+	var output string
+	if logger.NoColor {
+		output = logger.Mode + "\t\t" + logger.Module + "\t\t" + logger.Target + "\t\t" + str + "\n"
+	} else {
+		output = color.HiCyanString(logger.Mode+"\t\t"+logger.Module) + "\t\t" + logger.Target + "\t\t" + str + "\n"
+	}
 	//fmt.Println(output)
 	// For Linux and Windows support of colored output
 	fmt.Fprint(color.Output, output)
+	if logger.File != "" {
+		f, err := os.OpenFile(logger.File, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			logger.File = ""
+			logger.Error("Fail to open " + logger.File + ": " + err.Error())
+			return
+		}
+
+		defer f.Close()
+		output = strings.ReplaceAll(output, "\t\t", ";")
+		now := time.Now().Local()
+		if _, err = f.WriteString(now.Local().String() + ";" + output); err != nil {
+			logger.File = ""
+			logger.Error("Fail to write into " + logger.File + ": " + err.Error())
+			return
+		}
+	}
 
 }
