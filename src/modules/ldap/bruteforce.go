ba961f8f1cff4cffbbce2b1d2ba1892f48b03e1c
nodauf <nodauf@krutt.org>
2022-05-07 20:06:43 +0400

    Avoid ldap error if the creds are not valid

--- src/modules/ldap/bruteforce.go
+++ src/modules/ldap/bruteforce.go
@@ -1,9 +1,11 @@
 package ldap
 
+import "github.com/go-ldap/ldap/v3"
+
 func Authenticate(optionsInterface *interface{}, username, password string) bool {
 	options := (*optionsInterface).(*Options)
 	valid, err := options.authenticate(username, password)
-	if err != nil {
+	if err != nil && !ldap.IsErrorWithCode(err, ldap.LDAPResultInvalidCredentials) {
 		options.Log.Error("LDAP error: %s", err)
 	}
 	options.ldapConn.Close()
