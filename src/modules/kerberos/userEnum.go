5e75bc300f57a0609ef9249d100fa3408ab7b451
nodauf <nodauf@krutt.org>
2022-05-08 11:45:18 +0400

    Update logs

--- src/modules/kerberos/userEnum.go
+++ src/modules/kerberos/userEnum.go
@@ -11,9 +11,9 @@ func UserEnum(optionsInterface *interface{}, username string) bool {
 		// This is to determine if the error is "okay" or if we should abort everything
 		ok, errorString := handleKerbError(err)
 		if ok {
-			options.Log.Debug("%s %s", username, errorString)
+			options.Log.Debug("%s - %s", username, errorString)
 		} else {
-			options.Log.Fatal("%s %s", username, errorString)
+			options.Log.Fatal("%s - %s", username, errorString)
 		}
 	} else {
 		options.Log.Debug("Unknown behavior for username %s", username)
