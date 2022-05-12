94fe251a008edeaeba9afca2dad667669f551276
nodauf <nodauf@krutt.org>
2022-05-12 16:26:30 +0400

    Always check the tenant for the user enumeration

--- src/cmd/o365/brute.go
+++ src/cmd/o365/brute.go
@@ -36,8 +36,6 @@ By default, if one account is being lock, the all attack will be stopped.
 		orchestratorOptions.CustomOptionsForCheckIfValid = o365.PrepareOptions
 		orchestratorOptions.AuthenticationFunc = o365.Authenticate
 		orchestratorOptions.UserEnumFunc = o365.UserEnum
-		// To check if the user is valid
-		orchestratorOptions.CheckBeforeEnumFunc = o365.CheckTenant
 		validUsers = orchestratorOptions.Bruteforce(&o365Options)
 	},
 }
