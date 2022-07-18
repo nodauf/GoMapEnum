999f923ad256ed1caf073285c9e377b55d482cb1
nodauf <nodauf@krutt.org>
2022-07-18 12:32:47 +0200

    Fix logging

--- src/modules/kerberos/userEnum.go
+++ src/modules/kerberos/userEnum.go
@@ -1,7 +1,6 @@
 package kerberos
 
 func UserEnum(optionsInterface *interface{}, username string) bool {
-	valid := false
 	options := (*optionsInterface).(*Options)
 	valid, err := options.testUsername(username)
 	if valid {
@@ -12,11 +11,11 @@ func UserEnum(optionsInterface *interface{}, username string) bool {
 		options.Log.Debug(err.Error())
 		ok, errorString := handleKerbError(err)
 		if ok {
-			options.Log.Debug("%s - %s", username, errorString)
+			options.Log.Verbose("%s - %s", username, errorString)
 		} else {
 			options.Log.Fatal("%s - %s", username, errorString)
 		}
-	} else {
+	} else if !valid {
 		options.Log.Debug("Unknown behavior for username %s", username)
 	}
 
